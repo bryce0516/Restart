4cfc03d9b6e238e525a54812cfe79768f92c6f35
hyesungpark <jayhspark@gmail.com>
2023-08-22 21:50:37 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Hye Sung Park on 2023/08/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
