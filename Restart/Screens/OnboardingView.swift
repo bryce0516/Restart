13f8c6ba02d7a00f3f1c2b8de9e7c861fdf3d128
hyesungpark <jayhspark@gmail.com>
2023-08-23 22:39:09 +0900

    add capsule in footer

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -59,8 +59,41 @@ struct OnboardingView: View {
         
         // mark: - footer
         ZStack {
+          Capsule()
+            .fill(Color.white.opacity(0.2))
+          Capsule()
+            .fill(Color.white.opacity(0.2))
+            .padding()
+          
+          Text("Get Started")
+          HStack{
+            Capsule()
+              .fill(Color("ColorRed"))
+              .frame(width: 80)
+            
+            Spacer()
+          }
+          
+          HStack {
+            ZStack {
+              Circle()
+                .fill(Color("ColorRed"))
+              Circle()
+                .fill(.black.opacity(0.15))
+                .padding(8)
+              Image(systemName: "chevron.right.2")
+                .font(.system(size: 24, weight: .bold))
+            }
+            .foregroundColor(.white)
+            .frame(width: 80, height: 80, alignment: .center)
+            Spacer()
+          }
+          
           
         }
+        .frame(height: 80, alignment: .center)
+        .padding()
+        //: footer
       } //: vstack
     } //: zstack
   }
